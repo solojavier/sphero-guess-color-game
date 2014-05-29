031d52dffc99f97c283c8a528279f1445303a33a
Javier Cervantes <1.27201@gmail.com>
2014-05-29 15:15:24 -0500

    Making direct connection to serial port

--- Gemfile
+++ Gemfile
@@ -1,5 +1,8 @@
+source 'https://rubygems.org'
+
 gem 'sinatra'
 gem 'rspec'
 gem 'artoo'
 gem 'artoo-sphero'
+gem 'hybridgroup-serialport'
 gem 'rest-client'
--- sphero.rb
+++ sphero.rb
@@ -2,7 +2,7 @@ require 'artoo/robot'
 
 class SpheroRobot < Artoo::Robot
 
-  connection :sphero, :adaptor => :sphero, :port => '127.0.0.1:1234'
+  connection :sphero, :adaptor => :sphero, :port => '/dev/tty.Sphero-YBW-RN-SPP'
   device :sphero, :driver => :sphero
   api :host => '127.0.0.1', :port => '4321'
 
